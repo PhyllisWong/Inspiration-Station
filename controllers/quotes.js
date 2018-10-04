33f4144040343ec2c9a95e5561f522e3ff7c7be4
Phyllis Wong <wong.phyllism@gmail.com>
2018-10-04 13:25:15 -0700

    Add json object for parsing quotes

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -2,9 +2,7 @@
 
 const express = require('express');
 const app = express.Router();
-// const PDK = require('node-pinterest');
-// const pinterest = PDK.init(process.env.APP_SECRET);
-// pinterest.api('me').then(console.log);
+
 
 app.get('/', (req, res)=> {
   res.send("Hello Inspirational Quotes");
