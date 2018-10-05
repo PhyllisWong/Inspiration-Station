cde45c3666eabb80f0c5f8f6a729b6be16695c95
Phyllis Wong <wong.phyllism@gmail.com>
2018-10-04 17:20:55 -0700

    Add templates, parse json and send to route. WIP: send json to template

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -2,22 +2,24 @@
  * Quotes controller
  **************************************/
 
-// const quotes = require('../json/quotes');
-const fs = require('fs');
-
-fs.readFile('./json/quotes.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  const obj = JSON.parse(data);
-  console.log(obj[0].quote)
-});
-
 module.exports = function(app) {
 
+  // const quotes = require('../json/quotes');
+  const fs = require('fs');
+
+  var quotesArray = [];
+
+  fs.readFile('./json/quotes.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    quotesArray = JSON.parse(data);
+    console.log(quotesArray[0].quote)
+  });
+
   app.get('/', (req, res) => {
-    res.send("Hello Inspiration Station")
+    res.render("index")
   });
 
   app.get('/quotes', (req, res)=> {
-    res.json(quotes);
+    res.json(quotesArray);
   });
 };
