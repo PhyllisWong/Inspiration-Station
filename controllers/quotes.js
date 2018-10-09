373b8740ca23008f22e0a8976200091dcf166e92
Phyllis Wong <wong.phyllism@gmail.com>
2018-10-09 12:46:23 -0700

    Able to send random quote to template and display on the image

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -7,18 +7,20 @@ module.exports = function(app) {
   // const quotes = require('../json/quotes');
   const fs = require('fs');
 
-  var quotesArray = [];
+  let quotesArray = [];
+  let randomQuote = "";
 
   fs.readFile('./json/quotes.json', 'utf-8', (err, data) => {
     if (err) throw err;
     quotesArray = JSON.parse(data);
     let int = Math.floor(Math.random() * quotesArray.length );
     // Send this to a template
-    console.log(quotesArray[int].quote) // <----- confirmed this returns a quote
+    // console.log(quotesArray[int].quote) // <----- confirmed this returns a quote
+    randomQuote = quotesArray[int].quote;
   });
 
   app.get('/', (req, res) => {
-    res.render("index")
+    res.render('index', { quote: randomQuote });
   });
 
   app.get('/quotes', (req, res)=> {
